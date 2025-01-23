0842b03ce2cd35c953523057e2ab117ff4e6f107
joby121 <140624741+joby121@users.noreply.github.com>
2025-01-23 08:47:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+name = input('What is your name?: ')
+age = input('How old are you?: ')
+
+
+age = int(age)
+age = age + 1
+
+
+print(f'Hello {name}!')
+print('HAPPY BIRTHDAY')
+print(f'You are now {age} years old')
